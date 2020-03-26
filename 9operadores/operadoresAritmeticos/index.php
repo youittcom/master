77772efb012cfe72221be563259d7db39cb692ff
youittcom <youittcom@gmail.com>
2020-03-26 13:48:11 +0100

    operadores aritmeticos

--- 9operadores/operadoresAritmeticos/index.php
+++ 9operadores/operadoresAritmeticos/index.php
@@ -1,1 +1,29 @@
 <?php
+/*
+ * OPREDAORES ARITMETICOS
+ * Son los que utilizaremos para realizar sumas,restas,
+ * divisiones, modulo, potencia....
+ *
+ * suma (+): realiza la suma del valor de dos variables
+ *              $numero = 3;
+ *              $numero2 = 4;
+ *              $resultado = $numero + $numero2;
+ *
+ * resta(-): realiza la resta del valor de dos variable
+ *              $numero = 3;
+ *              $numero2 = 4;
+ *              $resultado = $numero - $numero2;
+ *
+ * producto(*): realiza el producto del valor de dos variable
+ *              $numero = 3;
+ *              $numero2 = 4;
+ *              $resultado = $numero * $numero2;
+ * division(/): realiza la division del valor de dos variable
+ *              $numero = 3;
+ *              $numero2 = 4;
+ *              $resultado = $numero / $numero2;
+ * modulo o resto(%): realiza el modulo de la division del valor de dos variable
+ *              $numero = 3;
+ *              $numero2 = 4;
+ *              $resultado = $numero % $numero2;
+ */
