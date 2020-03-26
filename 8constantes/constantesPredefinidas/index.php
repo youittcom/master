37a893c1e3b7327ce434a2bd9a21b5f00192c8f1
youittcom <youittcom@gmail.com>
2020-03-26 13:52:56 +0100

    info

--- 8constantes/constantesPredefinidas/index.php
+++ 8constantes/constantesPredefinidas/index.php
@@ -8,6 +8,7 @@
  *      PHP_OS; ---> version del sistema operativo.
  *      PHP_EXTENSION_DIR; ---> donde están instaladas las extensiones de PHP.
  *      __FILE__; ---> ruta del propio script.
+ * TODAS ESTAS CONSTANTES PREDEFNIDAS PUEDEN LLEGAR A SER MUY UTILES
  */
 
 PHP_OS;
