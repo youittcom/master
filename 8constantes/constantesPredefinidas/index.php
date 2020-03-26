083af34379db2f86ddae115e741f6bfcfe509b5f
youittcom <youittcom@gmail.com>
2020-03-26 13:32:18 +0100

    constantes

--- 8constantes/constantesPredefinidas/index.php
+++ 8constantes/constantesPredefinidas/index.php
@@ -1,1 +1,13 @@
 <?php
+/*
+ * CONSTANTES PREDEFINIDAS
+ * En php hay multitud de constantes predefinidas
+ * de las cuales podemos hacer uso.
+ *
+ *      PHP_VERSION; ---> version de PHP utilizada.
+ *      PHP_OS; ---> version del sistema operativo.
+ *      PHP_EXTENSION_DIR; ---> donde están instaladas las extensiones de PHP.
+ *      __FILE__; ---> ruta del propio script.
+ */
+
+PHP_OS;
